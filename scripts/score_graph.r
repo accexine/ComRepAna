b5c3255fb8e1d80ad82cec02b152e48cfeb74756
xdxf520 <xdxf520@vip.qq.com>
2013-10-30 23:41:46 +0800

    script

--- scripts/score_graph.r
+++ scripts/score_graph.r
@@ -1,36 +1,59 @@
 #setwd("D:\\graph\\")
-interval = 10; #时间间隔
+interval = 60; #时间间隔
 
-
-drawgraph <- function(datas,name){
-  datas$date = as.POSIXlt(datas$date);
-  min_date = min(datas$date);
-  max_date = max(datas$data);
-  counts.support = matrix(nrow=3,ncol=0);
-  begin_date = min_date;
-  end_date = min_date + interval * 60;
-  while(end_date<=max_date){
-    counts = computecount(begin_date,end_date,temp);
-    counts.support = cbind(counts.support,mid=counts);
-    begin_date = end_date;
-    end_date = end_date + interval * 60;
-  }
-  return (counts.support)
-  #  png(file=paste(name,'.png',sep=""));
-  #plot(datas$score,type="l",xlab=mid,ylab="")
-  #  dev.off()
+drawgraph <- function(matrix,mid){
+  print(paste(mid);
+  print(matrix);
 }
 
-computecount <- function(begin_date, end_date, datas){
-  counts = c(0,0,0) #c[1]为正，c[2]为负,c[3]为0.0
-  for(i in 1:length(datas$date)){
-    if(datas$score[i] > 0.0) counts[1] = counts[1] + 1
-    else if(datas$score[i] <0.0) counts[2] = counts[2] +1
-    else counts[3] = counts[3] + 1
-    if(datas$date[i]>end_data) break;
+getmatrix <- function(subset){
+  subset = subset[order(subset$date),];
+  subset$date = as.POSIXlt(subset$date);
+  begin.date = min(subset$date);
+  end.date = begin.date + interval * 60;
+  counts.support = matrix(nrow = 2, ncol = 0);
+  counts.names = c();
+  max.date = max(subset$date);
+  
+#  list.score = c();
+#  for(i in 1:length(subset$id)){
+#    if(subset[i,]$date < end.date){
+#      list.score[length(list.score)+1] = subset[i,]$score; 
+#    }else{
+#      if(length(list.score > 0)){
+#         counts = computecount(list.score);
+#         counts.support = cbind(counts.support,counts);
+#         counts.names[length(counts.names)+1] = begin.date;
+#         list.score = c();
+#         begin.date = end.date;
+#         end.data = end.date + interval * 60;
+#      }
+#    }
+# }
+  while(end.date <= max.date){
+    subset.subset = subset[begin.date <= subset$date,];
+    subset.subset = subset.subset[subset.subset$date < end.date,];
+    if(length(subset.subset$id) > 0){
+      counts = computecount(subset.subset$score);
+      counts.support = cbind(counts.support,"test"=counts);
+      counts.names[length(counts.names)+1] = paste(begin.date);
+   }
+    begin.date = end.date;
+    end.date = end.date + interval * 60;
   }
-  return (counts)
+  colnames(counts.support) = counts.names;
+  return (counts.support);
 }
+
+computecount <- function(score.list){
+  counts = c(0,0); #c[1] is positive, c[2] is negative
+  for(i in score.list){
+    if(i > 0) counts[1] = counts[1] + 1;
+    if(i < 0) counts[2] = counts[2] + 1; 
+  }
+  return (counts);
+}
+
 
 
 
@@ -39,21 +62,11 @@ data = read.table("D:\\score_results.txt",
            sep="\t", 
            col.names=c("source", "id", "mid", "date", "score"), 
            fill=TRUE, 
-           strip.white=TRUE)
-
-x = 1
-mid=0
-mid_list=c()
-for(i in 1:length(data$source)){
-  if(mid != data[i,3]){
-    mid = data[i,3]
-    mid_list[x] = mid
-    x = x+1
-  }
-}
-for(i in 1:length(mid_list)){
-  mid = mid_list[i]
-  temp = data[data$mid==mid,]
-  temp = temp[ order(temp$date), ]
-  drawgraph(temp,mid)
+           strip.white=TRUE);
+data$score = as.double(data$score);
+mid.list = unique(data$mid); #set all mid into mid.list
+for(i in mid.list){
+  data.subset = data[data$mid == i,];
+  matrix.counts = getmatrix(data.subset);
+  drawgraph(matrix.counts,i);
 }
